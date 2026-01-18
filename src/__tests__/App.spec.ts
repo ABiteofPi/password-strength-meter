4292f5910b0d02d54eedf2b4cd949a2500891f8e
ABiteofPi <payabiteofpi@gmail.com>
2026-01-18 18:55:08 +0100

    fix: updated vitest

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,11 +1,26 @@
 import { describe, expect, it } from 'vitest'
 
 import { mount } from '@vue/test-utils'
+import { createMemoryHistory, createRouter } from 'vue-router'
 import App from '../App.vue'
 
 describe('App', () => {
   it('mounts renders properly', () => {
-    const wrapper = mount(App)
-    expect(wrapper.text()).toContain('under construction')
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes: [
+        {
+          path: '/',
+          component: { template: '<div>This page is under construction</div>' },
+        },
+      ],
+    })
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router],
+      },
+    })
+    expect(wrapper.find('div').exists()).toBe(true)
   })
 })
