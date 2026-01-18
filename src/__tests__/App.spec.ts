a49cdf62b43cbfe0a899972ce5dd4b8a0d10bb10
ABiteofPi <payabiteofpi@gmail.com>
2026-01-18 18:57:04 +0100

    fix: updated vitest

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -5,7 +5,7 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import App from '../App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
+  it('mounts renders properly', async () => {
     const router = createRouter({
       history: createMemoryHistory(),
       routes: [
@@ -21,6 +21,10 @@ describe('App', () => {
         plugins: [router],
       },
     })
-    expect(wrapper.find('div').exists()).toBe(true)
+
+    await router.isReady()
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.text()).toContain('This page is under construction')
   })
 })
